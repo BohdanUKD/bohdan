d23d577dcd7d54e35d1610c68175c593f5bd0519
bohdanUKD <bohdan.r.pylypiv@ukd.edu.ua>
2024-04-22 09:33:22 +0300

    new file

---
+++ src/Dev.java
@@ -0,0 +1,12 @@
+public class Dev {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        devide(a, b);
+    }
+
+    public static void devide(int a, int b){
+        int c = a/b;
+        System.out.println(c);
+    }
+}
